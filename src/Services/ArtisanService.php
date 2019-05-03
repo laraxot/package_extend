d834c6f903434f467e89a1cf14938a1dd72ecea6
marco xot <marco.sottana@gmail.com>
2019-05-03 19:54:55 +0200

    artisanservice

--- src/Services/ArtisanService.php
+++ src/Services/ArtisanService.php
@@ -7,8 +7,25 @@ use Artisan;
 //----- TODO
 //--  1) capire come far fare da chiamato non da consolle "scout:import"
 
-class ArtisanService
-{
+class ArtisanService{
+
+    public static function  act($act){ //da fare anche in noconsole, e magari mettere un policy
+        switch($act){
+            case 'migrate'      : return ArtisanService::exe('migrate');
+            case 'routelist'    : return ArtisanService::exe('route:list');
+            case 'optimize'     : return ArtisanService::exe('optimize');
+
+            case 'clearcache'   : return ArtisanService::exe('cache:clear');
+            case 'routecache'   : return ArtisanService::exe('route:cache');
+            case 'routeclear'   : return ArtisanService::exe('route:clear');
+            case 'viewclear'    : return ArtisanService::exe('view:clear');
+            case 'configcache'  : return ArtisanService::exe('config:cache');
+            default             : return '';
+        }
+        return '';
+    }
+
+
     public static function exe($command, array $arguments = [])
     {
         try {
