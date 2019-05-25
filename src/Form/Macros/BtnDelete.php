583f80c1b48ca9b62911f2ae920c2dd5ab797fed
marco xot <marco.sottana@gmail.com>
2019-05-25 17:49:51 +0200

    .

--- src/Form/Macros/BtnDelete.php
+++ src/Form/Macros/BtnDelete.php
@@ -1,46 +1,50 @@
 <?php
 namespace XRA\Extend\Form\Macros;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
+//use Illuminate\Http\Request;
+//use Request;
 
 use Collective\Html\FormFacade as Form;
+//----- services -----
+use XRA\Extend\Services\ThemeService;
 
 
 class BtnDelete{
 	public function __invoke(){
         return function ($extra) {
-    $user=\Auth::user();
-    if($user==null) return '';
-    extract($extra);
+            $user=\Auth::user();
+            if($user==null) return '';
+            extract($extra);
 
-    if (!$user->can('delete', $row)){
-        return '[not can delete]['.class_basename($row).']';
-    }
-    //return '['.$row->destroy_url.']';
-    //$theme=\App\Services\ThemeService::class;
-    $routename=Request::route()->getName();
-    $routename=str_replace('.index_edit', '.destroy',$routename );
-    $routename=str_replace('.index', '.destroy',$routename );
+            if (!$user->can('delete', $row)){
+                return '[not can delete]['.class_basename($row).']';
+            }
+            //return '['.$row->destroy_url.']';
+            //$theme=\App\Services\ThemeService::class;
+            $routename=Request::route()->getName();
+            $routename=str_replace('.index_edit', '.destroy',$routename );
+            $routename=str_replace('.index', '.destroy',$routename );
 
-    //-----------
-    $id=$row->getKey();
-    //$id=array_values($extra)[0];
-    $params=\Route::current()->parameters();
-    $params=array_merge($params, $extra);
-    $params['routename']='';unset($params['routename']);
-    $route=route($routename, $params);
+            //-----------
+            $id=$row->getKey();
+            //$id=array_values($extra)[0];
+            $params=\Route::current()->parameters();
+            $params=array_merge($params, $extra);
+            $params['routename']='';unset($params['routename']);
+            $route=route($routename, $params);
 
-    //Theme::add('/theme/bc/jquery/dist/jquery.min.js');
-    Theme::add('theme/bc/sweetalert2/dist/sweetalert2.min.js');
-    Theme::add('theme/bc/sweetalert2/dist/sweetalert2.min.css');
-    Theme::add('extend::js/btnDeleteX2.js');
+            //Theme::add('/theme/bc/jquery/dist/jquery.min.js');
+            ThemeService::add('theme/bc/sweetalert2/dist/sweetalert2.min.js');
+            ThemeService::add('theme/bc/sweetalert2/dist/sweetalert2.min.css');
+            ThemeService::add('extend::js/btnDeleteX2.js');
 
-    $class='btn btn-small btn-danger';
-    if (isset($extra['class'])) {
-        $class.=' '.$extra['class'];
-    }
-    return '<a class="'.$class.'" href="#" data-token="'. csrf_token() .'" data-id="'.$id.'" data-href="'.$row->destroy_url.'?id='.$id.'" data-toggle="tooltip" title="Cancella">
-        <i class="fa fas fa-trash-alt fa-trash-o fa-fw" aria-hidden="true"></i>
-    </a>';
-}; //end function
+            $class='btn btn-small btn-danger';
+            if (isset($extra['class'])) {
+                $class.=' '.$extra['class'];
+            }
+            return '<a class="'.$class.'" href="#" data-token="'. csrf_token() .'" data-id="'.$id.'" data-href="'.$row->destroy_url.'?id='.$id.'" data-toggle="tooltip" title="Cancella">
+                <i class="fa fas fa-trash-alt fa-trash-o fa-fw" aria-hidden="true"></i>
+            </a>';
+        }; //end function
 	}//end invoke
 }//end class
