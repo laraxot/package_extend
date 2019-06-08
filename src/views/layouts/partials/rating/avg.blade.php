2de1573af2806af0b6ba69c9f4f4fcad02b70c39
marco xot <marco.sottana@gmail.com>
2019-06-08 19:16:37 +0200

    rich snippets

--- src/views/layouts/partials/rating/avg.blade.php
+++ src/views/layouts/partials/rating/avg.blade.php
@@ -6,12 +6,24 @@
 	$rating_url=route('container0.container1.index_edit',$parz);
 @endphp
 <div class="row">
-<div class="col-sm-8 col-md-8">
+<div class="col-sm-8 col-md-8" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
 	@php
 	$rating_avg=$row->ratings->avg('pivot.rating');
 	$rating_count=$row->ratings->count();
 	@endphp
+	<meta itemprop="ratingValue" content="{{ $rating_avg }}">
+	<meta itemprop="ratingCount" content="{{ $rating_count }}">
+	<meta itemprop="bestRating" content="5">
+	<meta itemprop="worstRating" content="1">
 	@include('extend::layouts.partials.rating.item',['label'=>'','rating_avg'=>$rating_avg,'rating_count'=>$rating_count])
+	{{-- item_type_schema_org  e microdate_schema_org son mutators non campi --}}
+	<div itemprop="itemReviewed" itemscope itemtype="{{ $row->item_type_schema_org }}" >
+		<meta itemprop="url" content="{{ $row->url }} " >
+		<meta itemprop="name" content="{{ $row->title }} " >
+		{!! $row->microdataSchemaOrg() !!}
+		
+	</div>
+
 </div>
 <div class="col-sm-4 col-md-4">
 	<button type="button" class="btn btn-red" data-toggle="modal" data-target="#myModalAjax" data-title="rate it" data-href="{{ $rating_url }}">
@@ -19,3 +31,6 @@
 	</button>
 </div>
 </div>
+
+
+	
